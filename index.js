551a61a5d5f2c32cb37bc76a9c773fefcaf280f7
Shoohag <shohag.sgazi@gmail.com>
2023-02-07 02:54:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// Problem - 1: 
+
+function mindGame(number){
+    let theProduct = number * 3;
+    let theSum = theProduct + 10;
+    let quotient = theSum / 2;
+    let subtraction = quotient - 5;
+    return subtraction;
+}
+
+
+
+//  end problem number 1
+
+// Problem - 2:
+
+
+function evenOdd(stringName){
+    let nameOfType = stringName.length;
+    if(nameOfType % 2 == 0){
+        return 'even';
+    }
+    else{
+        return 'odd';
+    }
+}
+
+
+// end problem number 2
+
+// problem- 3:
+
+function isLGSeven(number){
+    let quotient = number - 7;
+    let newQuotient = number *2;
+    return newQuotient;
+}
+
+// end problem number 3.
+
+// problem - 4:
+
+
---
+++ pactis.js
@@ -0,0 +1,90 @@
+function mindGame(number){
+    let theProduct = number * 3;
+    let theSum = theProduct + 10;
+    let quotient = theSum / 2;
+    let subtraction = quotient - 5;
+    return subtraction;
+}
+
+let total = mindGame(50);
+// console.log(total);
+
+
+// let evenOdd = 'pheroef';
+// let number = evenOdd.length;
+// console.log(number);
+// if(number % 2 == 0){
+//     console.log('even');
+// }
+// else{
+//     console.log('odd');
+// }
+
+function evenOdd(names){
+// let evenOdd = 'pheroef';
+let number = names.length;
+// console.log(number);
+if(number % 2 == 0){
+    // console.log('even');
+    return 'even';
+}
+else{
+    // console.log('odd');
+    return 'odd';
+}
+}
+
+let nameof = evenOdd('chatgpt');
+// console.log(nameof);
+
+
+
+// problem 3
+
+
+function isLGSeven(number){
+    let quotient = number - 7;
+    let newQuotient = number *2;
+    // console.log(newQuotient);
+}
+
+isLGSeven(7);
+
+
+
+// problem 4
+
+
+// let numbers = [-1, 2, -3, 4,5,6,-7,8,-9,10];
+// let count = 0;
+
+// for(let i = 0; i < numbers.length; i++){
+//     let newNumber = numbers[i];
+//     // console.log(newNumber);
+
+//     if(newNumber < 0){
+//         // console.log('bad');
+//         count = count+1;
+//     }
+//     else{
+//         // console.log('good');
+//     }
+// }
+
+// // console.log(count);
+
+
+// // way2
+
+
+function findingBadData(allData){
+   console.log(allData.length);
+}
+
+findingBadData[1 , 3 , -2, 4, -5, 5]
+
+function data(nns){
+    console.log(nns);
+}
+
+data[20];
